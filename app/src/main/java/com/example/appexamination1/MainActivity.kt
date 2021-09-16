67a6e09b6a84be0b3541c310c138f43fa8fe902e
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2021-09-16 14:38:39 +0200

    Pictures and layout

--- app/src/main/java/com/example/appexamination1/MainActivity.kt
+++ app/src/main/java/com/example/appexamination1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.appexamination1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        welcomeText = findViewById(R.id.welcomeText)
+        welcomeText.text = "Välkommen till ditt quiz! Här kan du välja mellan tre olika kategorier " +
+                "eller välja en blandning av alla tre. Lycka till!"
+
+        val buttonGeography = findViewById<Button>(R.id.geography)
+        buttonGeography.text = "Geografi"
+
+        val buttonScience = findViewById<Button>(R.id.science)
+        buttonScience.text = "Natur och vetenskap"
+
+        val buttonFoodAndBevereges = findViewById<Button>(R.id.foodBeverages)
+        buttonFoodAndBevereges.text = "Mat och dryck"
+
+        val buttonMixed = findViewById<Button>(R.id.mixed)
+        buttonMixed.text = "Blandat"
+
+
     }
 
-    welcomeText = findViewById(R.id)
+
 }
