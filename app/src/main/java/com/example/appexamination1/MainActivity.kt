99af4b6db17d8233bd8482ae448f7ee74018184c
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2021-09-16 13:43:43 +0200

    New project

---
+++ app/src/main/java/com/example/appexamination1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.appexamination1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var welcomeText : TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    welcomeText = findViewById(R.id)
+}
