20c1602dca29aae55df47ca56d0fb889bf4ecd23
Susanna Johansson <sannajohansson@MacBook-Air-som-tillhor-Susanna.local>
2021-09-18 13:34:17 +0200

    listOfOptions

--- app/src/main/java/com/example/appexamination1/Class Geography.kt
+++ app/src/main/java/com/example/appexamination1/Class Geography.kt
@@ -1,11 +1,8 @@
 package com.example.appexamination1
 
-    class Answer(
-        val option1: String,
-        val option2: String,
-        val option3: String,
-        val option4: String
-    )
+    class Question(val q: String, val listOfOption: Option, val a: String)
 
-    class Question(val q: String, val listOfAnswer: Answer)
+    class Option(val option1: String, val option2: String, val option3: String, val option4: String)
+
+
 
--- app/src/main/java/com/example/appexamination1/GeographyActivity.kt
+++ app/src/main/java/com/example/appexamination1/GeographyActivity.kt
@@ -1,41 +1,70 @@
 package com.example.appexamination1
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class GeographyActivity : AppCompatActivity() {
 
     lateinit var text: TextView
-    //val listOfAnswers = mutableListOf<Answer>() Vet ej om denna kommer behövas
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_geography2)
 
         val listOfQuestions = mutableListOf<Question>()
+        val listOfOption = mutableListOf<Option>()
 
 
-        val q1 = Question("Vad heter Nicaraguas huvudstad?", Answer("Managua", "San José",
-            "Tegucigalpa", "Buenos Aires") )
-        val q2 = Question("Vad heter Bolivias huvudstad?", Answer("Quito", "Sucre",
-            "Rio de Janeiro", "Lima") )
+        val q1 = Question(
+            "Vad heter Nicaraguas huvudstad?", o1(
+                "Managua", "San José",
+                "Tegucigalpa", "Buenos Aires"
+            ), "Managua"
+        )
+        val q2 = Question(
+            "Vad heter Bolivias huvudstad?", Option(
+                "Quito", "Sucre",
+                "Rio de Janeiro", "Lima"
+            ), "Sucre"
+        )
 
+        val o1 = Option ("Managua", "San José", "Tegucigalpa", "buenos Aires")
         listOfQuestions.add(q1)
         listOfQuestions.add(q2)
+
+        listOfOption.add(o1)
         //val randomQuestion = listOfQuestions.shuffled().first()
 
-        text = findViewById<TextView>(R.id.question)
-        text.text = "Här ska frågan stå!" //Typ: randomQuestion.q
+        val buttonOption1 = findViewById<Button>(R.id.option1)
+        val buttonOption2 = findViewById<Button>(R.id.option2)
+        val buttonOption3 = findViewById<Button>(R.id.option3)
+        val buttonOption4 = findViewById<Button>(R.id.option4)
+
         // showNewQuestion()
+
+
+        fun showNewQuestion() {
+
+            val randomQuestion = listOfQuestions.shuffled().first()
+            text = findViewById<TextView>(R.id.question)
+            text.text = randomQuestion.q
+
+            val options =
+            buttonOption1.text = randomQuestion.
+
+
+        }
+
+        //val questions = mutableListOf<Question>()
+
+        //val randomQuestion = mutableListOf<Question>().shuffled().first()
+
+        //fun showNewQuestion() {
+        //  randomQuestion
     }
-
-    //val questions = mutableListOf<Question>()
-
-    //val randomQuestion = mutableListOf<Question>().shuffled().first()
-
-    //fun showNewQuestion() {
-    //  randomQuestion
 }
 
 //fun showNewQuestion() {
